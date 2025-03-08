e5264a52896001037706b534280f31f040c2bc91
root <root@DESKTOP-QFEA9LV.>
2025-03-08 02:35:56 +0200

    create function for conventions binary numbers

--- assembler.h
+++ assembler.h
@@ -8,3 +8,6 @@ void check_file(int argc, char *argv[]);
 void read_file_line(char *filename);
 void create_file(char *file_name, char *file_type);
 void append_line_to_file(char *string, char *full_file_name);
+char *decimal_to_binary(int decimal_number, int bits_number);
+char convert_binary_to_hex(char *binary_number);
+char *convert_word_binary_to_hex(char *binary_number);
